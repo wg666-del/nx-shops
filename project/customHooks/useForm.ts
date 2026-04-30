7c5a42a2c295f0f7009801fe163d16f0d8f6aceb
wanggang2 <wanggang2@yxt.wecom.work>
2026-04-30 14:19:05 +0800

    feat: 字段调整

--- project/customHooks/useForm.ts
+++ project/customHooks/useForm.ts
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const useForm = (initValue) => {
+const useForm = (initValue = {}) => {
   const [values, setValues] = useState(initValue);
 
   const handleChange = (event) => {
